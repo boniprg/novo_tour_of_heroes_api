1a7b3871f80adeaed579fa8a9fe0f26635b9607f
boniprg <boniprg@gmail.com>
2023-09-12 18:38:47 -0300

    Valida campo name

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,2 +1,6 @@
 class Hero < ApplicationRecord
+    validates :name, presence: true 
+    validates :name, uniqueness: { case_sensitive: false }
+
+    scope :sorted_by_name, -> { order(:name) }
 end
